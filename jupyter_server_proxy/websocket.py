d660592c666ecd932f38b7a92b281d60db7bac8b
Patrick Hüther <huether@imb-mainz.de>
2025-02-10 16:13:09 +0100

    bump websocket max_message_size

--- jupyter_server_proxy/websocket.py
+++ jupyter_server_proxy/websocket.py
@@ -49,7 +49,7 @@ def pingable_ws_connect(
         compression_options={},
         on_message_callback=on_message_callback,
         on_ping_callback=on_ping_callback,
-        max_message_size=(100 * 1024 * 1024) # 100 MB
+        max_message_size=(100 * 1024 * 1024), # 100 MB
         subprotocols=subprotocols,
         **kwargs,
     )
